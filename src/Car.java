854cbd28dac8d7fe95b923a78bb0393c1be89f8e
northinrtm <artsdurden@gmail.com>
2022-09-21 13:28:36 +0300

    task3

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    private String brand;
+    private String model;
+    private float engineVolume;
+    private String color;
+    private int productionYear;
+    private String productionCountry;
+
+    public Car(String brand, String model, float engineVolume, String color, int productionYear, String productionCountry) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
+    }
+
+    @Override
+    public String toString() {
+        return "brand = " + brand + ", model = " + model + ", engineVolume = " + engineVolume +
+                ", color = " + color + ", productionYear = " + productionYear + ", productionCountry = " + productionCountry;
+    }
 }
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Car c1 = new Car("Lada","Grande", 1.7f, "желтая", 2015, "Россия");
+        Car c2 = new Car("Audi", "A8 50 L quattro", 3.0f, "черная", 2020, "Германия");
+        Car c3 = new Car("BMV", "Z8", 3.0f, "черный", 2021, "Германия");
+        Car c4 = new Car("Kia", "Sportage 4", 2.4f, "красный", 2018, "Южная Корея");
+        Car c5 = new Car("Hyundai", "Avante", 1.6f, "оранжевый", 2016, "Южная Корея");
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+        System.out.println(c5);
+    }
+}

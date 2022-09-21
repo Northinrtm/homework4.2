2e6af01b959b3a1b5212b47acc55ea2ef57bd247
northinrtm <artsdurden@gmail.com>
2022-09-21 14:07:18 +0300

    task3

--- src/Car.java
+++ src/Car.java
@@ -7,11 +7,34 @@ public class Car {
     private String productionCountry;
 
     public Car(String brand, String model, float engineVolume, String color, int productionYear, String productionCountry) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.productionYear = productionYear;
+        if (brand == null) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume == 0.0f) {
+            this.engineVolume = 1.5f;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (productionYear == 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (productionCountry == null) {
+            this.productionCountry = "default";
+        }
         this.productionCountry = productionCountry;
     }
 
